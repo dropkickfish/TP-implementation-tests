ee10075cb43a1ab48423ea78a3ba5741a82b1142
b1jam <85496497+b1jam@users.noreply.github.com>
2024-03-21 12:21:24 +0100

    scss instead css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
-import "./assets/styles/css/index.css"
+import "./assets/styles/scss/index.scss"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
